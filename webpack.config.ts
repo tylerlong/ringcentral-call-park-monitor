40c417927cd50279956b273bae17bc18d8c1bfd2
Tyler Long <tyler4long@gmail.com>
2020-07-24 11:35:09 -0700

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+/* eslint-disable node/no-unpublished-import */
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import path from 'path';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: ['./src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RingCentral Call Park monitor',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
+
+export default [config];
